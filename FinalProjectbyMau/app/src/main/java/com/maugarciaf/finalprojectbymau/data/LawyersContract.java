cc8d234936d5fed8e35063664d6030f1cd28118a
MauGf <maugarciaflores@gmail.com>
2019-05-31 16:00:30 -0600

    Create LawyersContract.java

---
+++ FinalProjectbyMau/app/src/main/java/com/maugarciaf/finalprojectbymau/data/LawyersContract.java
@@ -0,0 +1,17 @@
+package com.maugarciaf.finalprojectbymau.data;
+
+import android.provider.BaseColumns;
+
+public class LawyersContract {
+
+    public static abstract class LawyerEntry implements BaseColumns {
+        public static final String TABLE_NAME ="lawyer";
+
+        public static final String ID = "id";
+        public static final String NAME = "name";
+        public static final String SPECIALTY = "specialty";
+        public static final String PHONE_NUMBER = "phoneNumber";
+        public static final String AVATAR_URI = "avatarUri";
+        public static final String BIO = "bio";
+    }
+}
