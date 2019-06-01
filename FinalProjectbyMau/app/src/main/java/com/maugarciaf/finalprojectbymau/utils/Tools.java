af6919672b692a84b0952c780feb36ac2fe7523b
MauGf <maugarciaflores@gmail.com>
2019-05-31 18:56:09 -0600

    Update Tools.java

--- FinalProjectbyMau/app/src/main/java/com/maugarciaf/finalprojectbymau/utils/Tools.java
+++ FinalProjectbyMau/app/src/main/java/com/maugarciaf/finalprojectbymau/utils/Tools.java
@@ -1,4 +1,19 @@
 package com.maugarciaf.finalprojectbymau.utils;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.Window;
+import android.view.WindowManager;
+
 public class Tools {
+
+    public static void setSystemBarTransparent(Activity act) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = act.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
 }
