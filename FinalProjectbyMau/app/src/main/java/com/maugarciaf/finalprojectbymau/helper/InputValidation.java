09295ae445171bd7a479cc1f7846ed6e5bffa42d
MauGf <maugarciaflores@gmail.com>
2019-06-02 18:29:40 -0600

    Update InputValidation.java

--- FinalProjectbyMau/app/src/main/java/com/maugarciaf/finalprojectbymau/helper/InputValidation.java
+++ FinalProjectbyMau/app/src/main/java/com/maugarciaf/finalprojectbymau/helper/InputValidation.java
@@ -12,23 +12,12 @@ public class InputValidation {
 
     private Context context;
 
-    /**
-     * constructor
-     *
-     * @param context
-     */
+
     public InputValidation(Context context) {
         this.context = context;
     }
 
-    /**
-     * method to check InputEditText filled .
-     *
-     * @param textInputEditText
-     * @param textInputLayout
-     * @param message
-     * @return
-     */
+
     public boolean isInputEditTextFilled(TextInputEditText textInputEditText, TextInputLayout textInputLayout, String message) {
         String value = textInputEditText.getText().toString().trim();
         if (value.isEmpty()) {
@@ -43,44 +32,6 @@ public class InputValidation {
     }
 
 
-    /**
-     * method to check InputEditText has valid email .
-     *
-     * @param textInputEditText
-     * @param textInputLayout
-     * @param message
-     * @return
-     */
-    public boolean isInputEditTextEmail(TextInputEditText textInputEditText, TextInputLayout textInputLayout, String message) {
-        String value = textInputEditText.getText().toString().trim();
-        if (value.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
-            textInputLayout.setError(message);
-            hideKeyboardFrom(textInputEditText);
-            return false;
-        } else {
-            textInputLayout.setErrorEnabled(false);
-        }
-        return true;
-    }
-
-    public boolean isInputEditTextMatches(TextInputEditText textInputEditText1, TextInputEditText textInputEditText2, TextInputLayout textInputLayout, String message) {
-        String value1 = textInputEditText1.getText().toString().trim();
-        String value2 = textInputEditText2.getText().toString().trim();
-        if (!value1.contentEquals(value2)) {
-            textInputLayout.setError(message);
-            hideKeyboardFrom(textInputEditText2);
-            return false;
-        } else {
-            textInputLayout.setErrorEnabled(false);
-        }
-        return true;
-    }
-
-    /**
-     * method to Hide keyboard
-     *
-     * @param view
-     */
     private void hideKeyboardFrom(View view) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
