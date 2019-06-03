fcb009789f747445961a209361f89a0802fc0a9e
MauGf <maugarciaflores@gmail.com>
2019-06-03 16:53:09 -0600

    Update LawyersActivity.java

--- FinalProjectbyMau/app/src/main/java/com/maugarciaf/finalprojectbymau/lawyers/LawyersActivity.java
+++ FinalProjectbyMau/app/src/main/java/com/maugarciaf/finalprojectbymau/lawyers/LawyersActivity.java
@@ -1,12 +1,16 @@
 package com.maugarciaf.finalprojectbymau.lawyers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.maugarciaf.finalprojectbymau.AboutActivity;
 import com.maugarciaf.finalprojectbymau.R;
 
 public class LawyersActivity extends AppCompatActivity {
@@ -29,6 +33,30 @@ public class LawyersActivity extends AppCompatActivity {
                     .add(R.id.lawyers_container, fragment)
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater ().inflate (R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar items_swipe clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId ();
+
+        if (id == R.id.action_settings) {
+            Intent intentSettings = new Intent (getApplicationContext (), AboutActivity.class);
+            intentSettings.setFlags (Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity (intentSettings);
+            return true;
+        }
+
+        return super.onOptionsItemSelected (item);
     }
 
 }
