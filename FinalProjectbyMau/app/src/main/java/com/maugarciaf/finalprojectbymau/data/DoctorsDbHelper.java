6f8dceec5aa4fa005059ea9f4999b6d05ee2df53
MauGf <maugarciaflores@gmail.com>
2019-06-10 13:28:48 -0600

    Update DoctorsDbHelper.java

--- FinalProjectbyMau/app/src/main/java/com/maugarciaf/finalprojectbymau/data/DoctorsDbHelper.java
+++ FinalProjectbyMau/app/src/main/java/com/maugarciaf/finalprojectbymau/data/DoctorsDbHelper.java
@@ -9,7 +9,7 @@ import com.maugarciaf.finalprojectbymau.model.Doctors;
 
 public class DoctorsDbHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "Lawyers.db";
+    public static final String DATABASE_NAME = "Doctors.db";
 
     public DoctorsDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
